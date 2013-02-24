113b4d4eff435b134eb333f904dbd3d7a2b36188
Denis Postu <dpostu@kno.com>
2013-02-24 23:08:11 +0200

    Solved programming question 4
    Have been forced to increase the stack size of the solver thread in
    order to accomodate for the large recursion call stacks

--- Coursera.Implementations/ProgrammingQuestions/Question04/GraphComponentsConnector.cs
+++ Coursera.Implementations/ProgrammingQuestions/Question04/GraphComponentsConnector.cs
@@ -6,12 +6,69 @@ namespace Coursera.Implementations.ProgrammingQuestions.Question04
 {
     public class GraphComponentsConnector
     {
-        private Random m_random;
-        private List<Edge> m_edges; 
+        private int m_vertex;
+        private Dictionary<int, List<int>> m_outgoing = new Dictionary<int, List<int>>();
+        private Dictionary<int, List<int>> m_incoming = new Dictionary<int, List<int>>();
+        private HashSet<int> m_visited = new HashSet<int>(); 
+        private Stack<int> m_exclusionStack = new Stack<int>();
 
+        public IEnumerable<List<int>> FindConnectedComponents(int a, int b, List<Edge> adjacencyList)
+        {
+            for (var i = a; i <= b; ++i)
+            {
+                m_outgoing.Add(i, new List<int>());
+                m_incoming.Add(i, new List<int>());
+            }
+
+            foreach (var edge in adjacencyList)
+            {
+                m_outgoing[edge.A].Add(edge.B);
+                m_incoming[edge.B].Add(edge.A);
+            }
+
+            for (var i = b; i >= a; --i)
+            {
+                if (m_visited.Contains(i)) continue;
+                DFS1(i);
+            }
+
+            m_visited.Clear();
+
+            while (m_exclusionStack.Any())
+            {
+                m_vertex = m_exclusionStack.Pop();
+                if (m_visited.Contains(m_vertex)) continue;
+                
+                var list = new List<int>();
+                DFS2(m_vertex, list);
+                yield return list;
+            }
+        }
+
+        private void DFS2(int i, List<int> list)
+        {
+            m_visited.Add(i);
+            list.Add(i);
+            foreach (var j in m_outgoing[i])
+            {
+                if (m_visited.Contains(j)) continue;
+                DFS2(j, list);
+            }
+        }
+
+        private void DFS1(int i)
+        {
+            m_visited.Add(i);
+            foreach (var j in m_incoming[i])
+            {
+                if (m_visited.Contains(j)) continue;
+                DFS1(j);
+            }
+            m_exclusionStack.Push(i);
+        }
     }
 
-    internal class Edge
+    public class Edge
     {
 
         public override int GetHashCode()
@@ -24,8 +81,8 @@ namespace Coursera.Implementations.ProgrammingQuestions.Question04
 
         public Edge(int a, int b)
         {
-            A = Math.Min(a, b);
-            B = Math.Max(a, b);
+            A = a;
+            B = b;
         }
 
         public int A { get; set; }
--- Coursera.Implementations/ProgrammingQuestions/Question04/Runner.cs
+++ Coursera.Implementations/ProgrammingQuestions/Question04/Runner.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Coursera.Core;
 using Coursera.Core.Attributes;
@@ -14,9 +15,22 @@ namespace Coursera.Implementations.ProgrammingQuestions.Question04
     {
         public Task Run()
         {
+            var t = new Thread(InternalRun, 200 << 20);
+            t.Start();
+            t.Join();
+
+            return Task.FromResult(true);
+        }
+
+        private void InternalRun()
+        {
             var readingInputFileTask = "Reading input data";
+            var computingSccsTask = "Computing SCCs";
             var adjacencyList = new List<Edge>();
             var numbers = new int[2];
+            var connector = new GraphComponentsConnector();
+            var min = int.MaxValue;
+            var max = int.MinValue;
 
             Benchmarker.Start(readingInputFileTask);
             using (var reader = new StreamReader("ProgrammingQuestions/Question04/input.txt"))
@@ -26,12 +40,21 @@ namespace Coursera.Implementations.ProgrammingQuestions.Question04
                 {
                     numbers = line.Split(new[] {' ', '\t'}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
                     adjacencyList.Add(new Edge(numbers[0], numbers[1]));
+                    min = Math.Min(min, Math.Min(numbers[0], numbers[1]));
+                    max = Math.Max(max, Math.Max(numbers[0], numbers[1]));
                 }
             }
             Benchmarker.Stop(readingInputFileTask);
             Console.WriteLine("{1} took {0}ms", Benchmarker.EllapsedMilliseconds(readingInputFileTask), readingInputFileTask);
 
-            return Task.FromResult(true);
+            Benchmarker.Start(computingSccsTask);
+            var components = connector.FindConnectedComponents(min, max, adjacencyList).ToList();
+            Benchmarker.Stop(computingSccsTask);
+
+            Console.WriteLine("{1} took {0}ms", Benchmarker.EllapsedMilliseconds(computingSccsTask), computingSccsTask);
+            Console.WriteLine("Sizes of the largest components are: {0}", string.Join(",", components.Select(l => l.Count).OrderByDescending(l => l).Take(5)));
+            Console.WriteLine("Total size of components is: {0}", components.Select(c => c.Count).Sum());
+
         }
     }
 }
